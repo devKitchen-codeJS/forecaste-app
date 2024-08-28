d5e5a027f9094e58bfa4ae1dfdcb23c886542918
Sergey <smagometov649@gmail.com>
2024-08-28 08:34:23 +0300

    fix env issue

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -6,14 +6,24 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_APP_MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyDMxqTbsa12MRR4PMBvdn4jTB-7gTf5OFc",
+  authDomain: "forecast-focus-df15d.firebaseapp.com",
+  projectId: "forecast-focus-df15d",
+  storageBucket: "forecast-focus-df15d.appspot.com",
+  messagingSenderId: "728304121488",
+  appId: "1:728304121488:web:89d6534973d9e1a88f4a15",
+  measurementId: "G-00V42GMYKT",
 };
 console.log("api", process.env.NEXT_PUBLIC_APP_API_KEY);
 // Initialize Firebase
